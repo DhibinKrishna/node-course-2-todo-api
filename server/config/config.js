3e9de23dc977698639d268e411d9976f46a75dfa
dhibin <dhibink@gmail.com>
2019-01-21 23:37:56 +0530

    Add tests for GET /users/me and POST /users

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,12 @@
 var env = process.env.NODE_ENV || 'development'; //In production, this will be set to 'production' by Heroku, for testing this will be set to 'test' while running using mocha (package.json)
-//console.log('env ******', env);
+console.log('env ******', env);
 
 if(env === 'development') {
     process.env.port = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
 } else if (env === 'test') {
     process.env.port = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    // process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
 }
